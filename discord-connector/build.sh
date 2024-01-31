ee0f04328b1e0b540c30e99aac5a5788285f2285
aatomu <aatomu21263+git@gmail.com>
2024-02-01 00:06:40 +0900

    build message追加

--- discord-connector/build.sh
+++ discord-connector/build.sh
@@ -1,4 +1,7 @@
+go clean -cache -testcache
+
 echo Build Win64
 GOOS=windows GOARCH=amd64 go build -o ./build/DiscordConnector-Win64.exe -ldflags="-s -w -H=windowsgui" -trimpath *.go
 echo Build Linux64
 GOOS=linux GOARCH=amd64 go build -o ./build/DiscordConnector-Linux64.exe -ldflags="-s -w" -trimpath *.go
+echo Build Finish
