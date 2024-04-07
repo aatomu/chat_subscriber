6017e9933324af20f7b00c6387c79cf8fbae77a8
aatomu <aatomu21263+git@gmail.com>
2024-04-07 18:50:38 +0900

    ためしこみっと

--- pages/functions/example.ts
+++ pages/functions/example.ts
@@ -2,6 +2,19 @@ interface Env {
 
 }
 
-export const onRequest: PagesFunction<Env> = async (context) => {
-  return new Response("aaaa")
+// export const onRequest: PagesFunction<Env> = async (context) => {
+//   return new Response("aaaa")
+// }
+
+export default {
+  async onRequest(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    return new Response("aaaa")
+  }
 }
+export async function onRequest(ctx:ExecutionContext) {
+}
+
+// export default {
+// 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+//   }
+// }
