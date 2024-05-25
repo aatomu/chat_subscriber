6920209eb4878daff8094b60ab0e2464789c07a5
aatomu <aatomu21263+git@gmail.com>
2024-05-25 23:28:18 +0900

    index追加

---
+++ pages/discord/assets/index.js
@@ -0,0 +1,16 @@
+/// <reference path="voice.js"/>
+/// <reference path="channel.js"/>
+/// <reference path="main.js"/>
+
+/**
+ * @typedef User
+ * @type {object}
+ * @property {number} id
+ * @property {string} username
+ * @property {string} global_name
+ * @property {string} avatar
+ * @property {object} avatar_decoration_data
+ * @property {string} bot
+ * @property {number} flags
+ * @property {number} premium_type
+*/
